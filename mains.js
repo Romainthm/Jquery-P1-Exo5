de08eb3f775a2f26daea2cbd28016d8dc7856dce
romain <romain.thomine007@gmail.com>
2019-04-19 10:03:20 +0200

    fini

---
+++ mains.js
@@ -0,0 +1,9 @@
+$( "#btn1" ).click(function() {
+  $( "#firstText" ).empty();
+});
+$( "#btn2" ).click(function() {
+  $( "#secondText" ).empty();
+});
+$( "#btn3" ).click(function() {
+  $( "#thirdText" ).empty();
+});
